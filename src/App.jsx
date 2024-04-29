06e8394924e5f37acab5765da9e202263452911d
Narmin2004 <narminbabayeva04@gmail.com>
2024-04-29 12:33:16 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import First from './Components/First'
+import Second from './Components/Second'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <First />
+        <Second />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/First.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./First.css"
+
+function First() {
+    return (
+        <div>
+            <h1>First</h1>
+        </div>
+    )
+}
+
+export default First
